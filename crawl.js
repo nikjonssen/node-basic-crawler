93503edb259713c56d0da988a808cb0656150625
Nik Jones <nikjonssen@gmail.com>
2023-07-28 17:09:48 +0300

    getURLsFromHTML and tests

--- crawl.js
+++ crawl.js
@@ -1,3 +1,31 @@
+const { JSDOM } = require("jsdom");
+
+function getURLsFromHTML(htmlBody, baseURL) {
+  const urls = [];
+  const urlObj = new URL(baseURL);
+  const origin = urlObj.origin;
+  const dom = new JSDOM(htmlBody);
+  const linkElements = dom.window.document.querySelectorAll("a");
+  for (el of linkElements) {
+    if (el.href.slice(0, 1) === "/") {
+      try {
+        const isURL = new URL(`${origin}${el.href}`);
+        urls.push(isURL.href);
+      } catch (error) {
+        console.log(`Relative url validation failed: ${error.message}`);
+      }
+    } else {
+      try {
+        const isURL = new URL(`${el.href}`);
+        urls.push(isURL.href);
+      } catch (error) {
+        console.log(`Absolute url validation failed: ${error.message}`);
+      }
+    }
+  }
+  return urls;
+}
+
 function normalizeURL(urlString) {
   const urlObj = new URL(urlString);
   const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
@@ -9,4 +37,5 @@ function normalizeURL(urlString) {
 
 module.exports = {
   normalizeURL,
+  getURLsFromHTML,
 };
--- crawl.test.js
+++ crawl.test.js
@@ -1,5 +1,5 @@
 const { test, expect } = require("@jest/globals");
-const { normalizeURL } = require("./crawl");
+const { normalizeURL, getURLsFromHTML } = require("./crawl");
 
 test("normalizeURL remove https protocol", () => {
   const input = "https://brannmarkt.netlify.app/about";
@@ -22,9 +22,69 @@ test("normalizeURL remove trailing slash", () => {
   expect(actual).toEqual(expected);
 });
 
-test("normalizeURL capitals", () => {
+test("normalizeURL capital letters", () => {
   const input = "https://BrAnNmarkt.netlify.app/";
   const actual = normalizeURL(input);
   const expected = "brannmarkt.netlify.app";
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML absolute", () => {
+  const inputHTMLBody = `
+    <html>
+      <body>
+        <a href="https://brannmarkt.netlify.app/path/"></a>
+      </body>
+    </html>
+  `;
+  const inputBaseURL = "https://brannmarkt.netlify.app/path/";
+  const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+  const expected = ["https://brannmarkt.netlify.app/path/"];
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML relative", () => {
+  const inputHTMLBody = `
+    <html>
+      <body>
+        <a href="/path/"></a>
+      </body>
+    </html>
+  `;
+  const inputBaseURL = "https://brannmarkt.netlify.app/";
+  const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+  const expected = ["https://brannmarkt.netlify.app/path/"];
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML absolute and relative", () => {
+  const inputHTMLBody = `
+    <html>
+      <body>
+      <a href="https://brannmarkt.netlify.app/path1/"></a>
+        <a href="/path2/"></a>
+      </body>
+    </html>
+  `;
+  const inputBaseURL = "https://brannmarkt.netlify.app/";
+  const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+  const expected = [
+    "https://brannmarkt.netlify.app/path1/",
+    "https://brannmarkt.netlify.app/path2/",
+  ];
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML invalid", () => {
+  const inputHTMLBody = `
+    <html>
+      <body>
+        <a href="invalid"></a>
+      </body>
+    </html>
+  `;
+  const inputBaseURL = "https://brannmarkt.netlify.app/";
+  const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+  const expected = [];
   expect(actual).toEqual(expected);
 });
