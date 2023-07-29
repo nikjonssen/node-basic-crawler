fed950d8dbe9ec70f2ff18c9e1c104210d6076c3
Nik Jones <nikjonssen@gmail.com>
2023-07-29 16:11:59 +0300

    crawlPage recursive refactor

--- crawl.js
+++ crawl.js
@@ -1,16 +1,28 @@
 const { JSDOM } = require("jsdom");
 
-async function crawlPage(currentURL) {
+async function crawlPage(baseURL, currentURL, pages) {
+  const baseURLObj = new URL(baseURL);
+  const currentURLObj = new URL(currentURL);
+  if (baseURLObj.hostname !== currentURLObj.hostname) {
+    return pages;
+  }
+
+  const normalizedCurrentURL = normalizeURL(currentURL);
+  if (pages[normalizedCurrentURL] > 0) {
+    pages[normalizedCurrentURL]++;
+    return pages;
+  }
+
+  pages[normalizedCurrentURL] = 1;
   console.log(`crawl active: ${currentURL}`);
 
   try {
     const resp = await fetch(currentURL);
-
     if (resp.status > 399) {
       console.log(
         `crawl fetch error, code: ${resp.status}, page: ${currentURL}`
       );
-      return;
+      return pages;
     }
 
     const contentType = resp.headers.get("content-type");
@@ -18,13 +30,19 @@ async function crawlPage(currentURL) {
       console.log(
         `crawl fetch non html response, content type: ${contentType}, page: ${currentURL}`
       );
-      return;
+      return pages;
     }
 
-    console.log(await resp.text());
+    const htmlBody = await resp.text();
+    const nextURLs = getURLsFromHTML(htmlBody, baseURL);
+    for (const nextURL of nextURLs) {
+      pages = await crawlPage(baseURL, nextURL, pages);
+    }
   } catch (error) {
     console.log(`crawl fetch failed: ${error.message}`);
   }
+
+  return pages;
 }
 
 function getURLsFromHTML(htmlBody, baseURL) {
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const { crawlPage } = require("./crawl");
 
-function main() {
+async function main() {
   if (process.argv.length < 3) {
     console.log("No website url provided");
     process.exit(1);
@@ -12,7 +12,11 @@ function main() {
   const baseURL = process.argv[2];
 
   console.log(`crawl started: ${baseURL}`);
-  crawlPage(baseURL);
+  const pages = await crawlPage(baseURL, baseURL, {});
+
+  for (const page of Object.entries(pages)) {
+    console.log(page);
+  }
 }
 
 main();
